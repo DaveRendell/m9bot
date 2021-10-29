2cb140fe6ffc26e5809f135d02aa741752917b35
David Rendell <davidwrendell@gmail.com>
2021-10-29 07:39:43 +0100

    Update help message

--- src/discordResponses/helpMessage.ts
+++ src/discordResponses/helpMessage.ts
@@ -3,5 +3,7 @@ import * as Discord from "discord.js"
 export default function helpMessage(message: Discord.Message): void {
   message.reply("Hi m9. Here's some usage info:\n"
     + "- To set your birthday, message `set birthday YYYY-MM-DD`\n"
-    + "- To pin a message, react with the 📌 (`pushpin`) emoji\n")
+    + "- To list upcoming birthdays, message `list birthdays`"
+    + "- To pin a message, react with the 📌 (`pushpin`) emoji\n"
+    + "See https://github.com/DaveRendell/m9bot for more info")
 }
