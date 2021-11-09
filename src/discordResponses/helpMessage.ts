b29103087fd7de758aa4e9f93d1832d9c35a4ea5
David Rendell <davidwrendell@gmail.com>
2021-11-09 21:51:01 +0000

    Fix help message

--- src/discordResponses/helpMessage.ts
+++ src/discordResponses/helpMessage.ts
@@ -6,7 +6,7 @@ export default function helpMessage(message: Discord.Message): void {
     + "- To set your birthday, message `set birthday YYYY-MM-DD`\n"
     + "- To list upcoming birthdays, message `list birthdays`"
     + "- To pin a message, react with the 📌 (`pushpin`) emoji\n"
-    + `- Self assign roles to view hidden channels in <#${config.discord.selfServiceMessageChannelId}>`
-    + `- [Admin only] Add roles users can self assign with \`add_self_service_role [tag the role here] [emoji for the role] [description]\``
+    + `- Self assign roles to view hidden channels in <#${config.discord.selfServiceMessageChannelId}>\n`
+    + `- [Admin only] Add roles users can self assign with \`add_self_service_role [tag the role here] [emoji for the role] [description]\`\n`
     + "See https://github.com/DaveRendell/m9bot for more info")
 }
