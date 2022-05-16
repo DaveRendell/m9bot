06f4dc8fc2f0270f09ce13c87ef400f1d309b86f
Your Name <you@example.com>
2022-05-16 23:28:17 +0100

    help message

--- src/discordResponses/helpMessage.ts
+++ src/discordResponses/helpMessage.ts
@@ -4,7 +4,8 @@ import config from "src/config"
 export default function helpMessage(message: Discord.Message): void {
   message.reply("Hi m9. Here's some usage info:\n"
     + "- To set your birthday, message `set birthday YYYY-MM-DD`\n"
-    + "- To list upcoming birthdays, message `list birthdays`"
+    + "- To list upcoming birthdays, message `list birthdays`\n"
+    + "- To randomise the server name, message `m9bot shuffle!`\n"
     + "- To pin a message, react with the 📌 (`pushpin`) emoji\n"
     + `- Self assign roles to view hidden channels in <#${config.discord.selfServiceMessageChannelId}>\n`
     + `- [Admin only] Add roles users can self assign with \`add_self_service_role [tag the role here] [emoji for the role] [description]\`\n`
